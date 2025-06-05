06dda8eea226d6767f0098e3077d1add0de21859
JafarAbdi <jafar.uruc@gmail.com>
2025-06-05 14:23:21 +0100

    Fix joint constraints for custom models

--- src/placo/model/robot_wrapper.cpp
+++ src/placo/model/robot_wrapper.cpp
@@ -14,8 +14,20 @@
 #include <json/json.h>
 #include <filesystem>
 #include <algorithm>
+#include <regex>
 
 namespace fs = boost::filesystem;
+
+namespace {
+inline const auto kPinocchioUnboundedJoint = std::regex("JointModelRUB([XYZ])$");
+inline const auto kPinocchioRevoluteUnboundedUnalignedJoint =
+    std::string_view("JointModelRevoluteUnboundedUnaligned");
+
+bool is_continuous_joint(const pinocchio::JointModel& joint) {
+  return std::regex_match(joint.shortname(), kPinocchioUnboundedJoint) ||
+         joint.shortname() == kPinocchioRevoluteUnboundedUnalignedJoint;
+}
+}
 
 namespace placo::model
 {
@@ -144,12 +156,25 @@ pinocchio::FrameIndex RobotWrapper::get_frame_index(const std::string& frame)
 
 void RobotWrapper::set_joint(const std::string& name, double value)
 {
-  state.q[get_joint_offset(name)] = value;
+  const auto joint_index = get_joint_offset(name);
+  if (is_continuous_joint(model.joints[model.getJointId(name)]))
+  {
+    state.q[joint_index] = std::cos(value);
+    state.q[joint_index + 1] = std::sin(value);
+  }
+  else {
+    state.q[joint_index] = value;
+  }
 }
 
 double RobotWrapper::get_joint(const std::string& name)
 {
-  return state.q[get_joint_offset(name)];
+  const auto joint_index = get_joint_offset(name);
+  if (is_continuous_joint(model.joints[model.getJointId(name)]))
+  {
+    return std::atan2(state.q[joint_index + 1], state.q[joint_index]);
+  }
+  return state.q[joint_index];
 }
 
 int RobotWrapper::get_joint_offset(const std::string& name)
@@ -161,12 +186,12 @@ int RobotWrapper::get_joint_offset(const std::string& name)
     throw std::runtime_error(oss.str());
   }
 
-  return 7 + model.getJointId(name) - 2;
+  return model.joints[model.getJointId(name)].idx_q();
 }
 
 int RobotWrapper::get_joint_v_offset(const std::string& name)
 {
-  return 6 + model.getJointId(name) - 2;
+  return model.joints[model.getJointId(name)].idx_v();
 }
 
 double RobotWrapper::get_joint_velocity(const std::string& name)
